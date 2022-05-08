5e41132c51d4a6addfb9a26bccc1c4dc3f2b51f9
LucasMaximiliano <lucas.maxi@uol.com.br>
2022-05-08 13:38:37 +0200

    Aufgabe 3b rekursiv gemacht.

--- U00/Aufgabe3b.cpp
+++ U00/Aufgabe3b.cpp
@@ -9,7 +9,7 @@ reverse(array, laenge)
 
 #include <stdio.h>
 
-void reverse(int* array, const int laenge)
+void reverse_iterative(int* array, const int laenge)
 {
     for(int i=0; i<(laenge/2); i++)
     {
@@ -19,13 +19,36 @@ void reverse(int* array, const int laenge)
     }
 }
 
+void reverse_recursive(int* array, const int laenge)
+{
+    static int org_laenge = laenge;
+    if(laenge == org_laenge/2)
+        return;
+    else
+    {
+        static int count = 0;
+        int buff = array[laenge-1];
+        array[laenge-1] = array[count];
+        array[count] = buff;
+        count++;
+        reverse_recursive(array, laenge-1);
+    }
+}
+
 int main()
 {
-    int a[5] = {0, 7, 12, 53, -40};      //Testlauf
-    reverse(a, 5);
+    int a[5] = {0, 7, 12, 53, -40};         //Testlauf 1
+    reverse_iterative(a, 5);
     for(int i=0; i<5; i++)
     {
         printf("%d ", a[i]);
+    }
+
+    int b[5] = {24, -30, -4, 75, 280};       //Testlauf 2
+    reverse_recursive(b, 5);
+    for(int i=0; i<5; i++)
+    {
+        printf("%d ", b[i]);
     }
 
     return 0;
