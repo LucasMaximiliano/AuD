3e1188574c519870a1f3b5bcfe02164e5f78928a
LucasMaximiliano <lm.deoliveiraesilva@gmail.com>
2022-05-11 18:54:05 +0000

    U02 Aufgabe1b hinzugefuegt.

---
+++ U02/Aufgabe1b.cpp
@@ -0,0 +1,28 @@
+/*PSEUDOCODE:
+ *pi(n)
+ *	if(n==0)
+ *		return 0
+ *	else if(n%2==0)
+ *		return 4/(2*n+1) + pi(n-1)
+ *	else
+ *		return -4/(2*n+1) + pi(n-1)
+ */
+
+#include<stdio.h>
+
+float pi(const int  n)
+{
+	if(n==0)
+		return 4;
+	else if(n%2==0)
+		return 4.0/(2*n+1) + pi(n-1);
+	else
+		return -4.0/(2*n+1) + pi(n-1);
+}
+
+int main()
+{
+	printf("%f, %f, %f\n", pi(5.0), pi(10.0), pi(100.0) );	//Testlaeufe
+	
+	return 0;
+}
