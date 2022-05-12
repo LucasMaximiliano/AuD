d99970b2c3fd67009423b579b36ade669c0db336
LucasMaximiliano <lm.deoliveiraesilva@gmail.com>
2022-05-12 15:42:14 +0000

    U02 Aufgabe2 hinzugefuegt.

---
+++ U02/Aufgabe2.cpp
@@ -0,0 +1,33 @@
+/* PSEUDOCODE:
+ * binomial_koeffizient(n,k)
+ * 	if n==k || k==0
+ * 		return 1
+ * 	else
+ * 		ergebnisse[n][k]
+ * 		if ergebnisse[n][k] != 0
+ * 			return ergebnisse[n][k]
+ * 		else
+ * 			return ergebnisse[n][k] = binomial_koeffizient(n-1,k-1) + binomial_koeffizient(n-1,k)
+ */
+
+#include <stdio.h>
+
+int binomial_koeffizient(const int n, const int k)
+{
+	if(n==k || k==0)
+		return 1;
+	else
+	{
+		static int ergebnisse[1000][1000];	//[0] wird nicht benutzt
+		if(ergebnisse[n][k]!=0)
+			return ergebnisse[n][k];
+		else
+			return ergebnisse[n][k] = binomial_koeffizient(n-1,k-1) + binomial_koeffizient(n-1,k);
+	}
+}
+
+int main()
+{
+	printf("%d", binomial_koeffizient(5,3) );	//Testlauf
+	return 0;
+}
